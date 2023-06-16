ed6840ee41caf4cd8e722c9bdab46dabccc8781e
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-16 09:28:10 +0200

    Update init-script.sh

--- ProdEnvironment/init-script.sh
+++ ProdEnvironment/init-script.sh
@@ -9,4 +9,4 @@ sudo curl -fsSL https://get.docker.com/ -o init-script.sh
 sudo sh init-script.sh
 
 sudo wget https://github.com/mhkrhn/MKbrief14.git
-sudo docker run -d --name mktest14 -p 1235:80 mhkrhn/mkbrief14
+sudo docker run -d --name mktest14 -p 1234:80 mhkrhn/mkbrief14
